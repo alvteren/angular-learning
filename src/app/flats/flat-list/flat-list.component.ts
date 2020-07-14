270d1465b412b12ecb70a20b187434373966f71c
Aleksandr <alvteren@gmail.com>
2020-07-14 09:40:44 +0300

    remake fetchState on flat list

--- src/app/flats/flat-list/flat-list.component.ts
+++ src/app/flats/flat-list/flat-list.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { Flat } from './flat.model';
 import {FlatListService} from './flat-list.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { PromiseState } from 'src/app/shared/promise-state.model';
 
 
 @Component({
@@ -10,15 +11,22 @@ import { Observable } from 'rxjs';
   templateUrl: './flat-list.component.html',
   styleUrls: ['./flat-list.component.css']
 })
-export class FlatListComponent implements OnInit {
-  list: Observable<Flat[]> | null = null;
-  fetchState: FlatListService['fetchState'];
+export class FlatListComponent implements OnInit, OnDestroy {
+  list$: Observable<Flat[]> | null = null;
+  fetchState: PromiseState;
+  fetchStateSubscription: Subscription;
 
   constructor(private flatListService: FlatListService) { }
 
   ngOnInit(): void {
-    this.list = this.flatListService.fetchList();
-    this.fetchState = this.flatListService.fetchState;
+    this.list$ = this.flatListService.fetchList();
+    this.fetchStateSubscription = this.flatListService.fetchState$.subscribe((state) => {
+      console.log(state)
+      this.fetchState = state
+    });
   }
 
+  ngOnDestroy() {
+    this.fetchStateSubscription.unsubscribe();
+  }
 }
--- src/app/flats/flat-list/flat-list.service.ts
+++ src/app/flats/flat-list/flat-list.service.ts
@@ -4,27 +4,33 @@ import { catchError, map, tap } from 'rxjs/operators';
 
 import { Flat, FlatResponse } from './flat.model';
 import {PromiseState} from '../../shared/promise-state.model';
-import { Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlatListService {
-  public fetchState = new Subject<PromiseState>();
+  public fetchState$ = new BehaviorSubject<PromiseState | undefined>(undefined);
 
   constructor(private http: HttpClient) {}
 
   fetchList = () => {
     return this.http.get<FlatResponse.Palyload>('https://www.sdvor.com/api/common/flats/')
-      .pipe<any, any, Flat[]>(
-        tap(()=> this.fetchState.next('pending')),
+      .pipe<FlatResponse.Palyload, any, Flat[], Flat[]>(
+        tap((arg)=> {
+          console.log('pending', arg)
+          this.fetchState$.next('pending')
+        }),
         catchError((err)=> {
-          this.fetchState.next('rejected');
+          this.fetchState$.next('rejected');
           throw err;
         }),
         map(({results})=> {
-          this.fetchState.next('fulfilled');
           return results.map(({id, category, city,address, square, price})=> ({id, category, city,address, square, price}));
+        }),
+        tap((arg)=>{
+          console.log('fulfilled', arg)
+          this.fetchState$.next('fulfilled')
         })
       )
   }
